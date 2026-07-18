34519f18513a930f44107559880de94bb9df742e
jcruz <jorge.cruz@alpha-fullcommerce.mx>
2026-07-18 00:09:47 -0600

    compartir: suscribir empleado como seguidor para que el proyecto salga en portal

--- maintegradora_custom/models/project_task.py
+++ maintegradora_custom/models/project_task.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import api, models, fields
+
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
@@ -8,3 +9,39 @@ class ProjectTask(models.Model):
         'project_task_employee_rel', 'task_id', 'employee_id',
         string='Empleados asignados',
     )
+
+    def _sync_employee_collaborators(self):
+        """Comparte el proyecto de la tarea (modo Editar) con el partner de cada empleado asignado."""
+        Collaborator = self.env['project.collaborator']
+        for task in self:
+            project = task.project_id
+            if not project:
+                continue
+            partners = task.employee_ids.mapped(
+                lambda e: e.work_contact_id or e.user_partner_id
+            )
+            if not partners:
+                continue
+            if project.privacy_visibility != 'portal':
+                project.privacy_visibility = 'portal'
+            existing = project.collaborator_ids.mapped('partner_id')
+            for partner in partners - existing:
+                Collaborator.create({
+                    'project_id': project.id,
+                    'partner_id': partner.id,
+                    'limited_access': False,  # Editar (acceso completo)
+                })
+            # Suscribir como seguidores: el portal (My Projects) filtra por follower
+            project.message_subscribe(partner_ids=partners.ids)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        tasks = super().create(vals_list)
+        tasks._sync_employee_collaborators()
+        return tasks
+
+    def write(self, vals):
+        res = super().write(vals)
+        if 'employee_ids' in vals:
+            self._sync_employee_collaborators()
+        return res
